ab65e5fb20c0a0690d2cd2735905910ff170feda
Wilmary <wilmarymercedes1404@gmail.com>
2025-02-05 16:35:58 -0400

    arrgle algo del texto

--- asset/js/app.js
+++ asset/js/app.js
@@ -1,10 +1,1 @@
 // Importa las funciones necesarias
-import { register } from "./inicio.js";
-
-
-// Ejecuta las funciones cuando el DOM esté completamente cargado
-document.addEventListener("DOMContentLoaded", () => {
-
-register();
-})
-
