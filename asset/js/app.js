b9578daef93b94df7afec368ceb7e961b0ae5076
Wilmary <wilmarymercedes1404@gmail.com>
2025-02-19 10:48:37 -0400

    instalando dependensias y entrando codigo

--- asset/js/app.js
+++ asset/js/app.js
@@ -1,1 +1,20 @@
-// Importa las funciones necesarias
+const express = require('express')
+const app = express()
+
+app.disable('x-powered-by')
+
+const PORT = process.env.PORT ?? 1234
+
+app.get('/', (req, res) => {
+  res.send('<h1>Hello World</h1>')
+})
+
+//manejo del error 404
+app.use((req, res)=>{
+  res.status(404).send(`<h1>404<h1/>`)
+  
+})
+
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`)
+})
---
+++ modole/mysql/usurio.js
@@ -0,0 +1,15 @@
+/* import mysql from 'mysql2/promise'
+
+const DEFAULT_CONFIG = {
+  host: 'localhost',
+  user: 'root',
+  port: 3306,
+  password: '',
+  database: 'bas_gatuna'
+}
+ const connectionString = process.env.DATABASE_URL ?? DEFAULT_CONFIG
+
+ const connection = await mysql.createConnection(connectionString)
+
+<<<<<<< Tabnine <<<<<<<
+ */
