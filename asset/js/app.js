bf7d34db99bbbce04a382b3c4c7b1eed0977258a
Wilmary <wilmarymercedes1404@gmail.com>
2025-03-04 11:02:29 -0400

    continuamos trabajando

--- asset/js/app.js
+++ asset/js/app.js
@@ -9,10 +9,9 @@ app.get('/', (req, res) => {
   res.send('<h1>Hello World</h1>')
 })
 
-//manejo del error 404
-app.use((req, res)=>{
-  res.status(404).send(`<h1>404<h1/>`)
-  
+// manejo del error 404
+app.use((req, res) => {
+  res.status(404).send('<h1>404<h1/>')
 })
 
 app.listen(PORT, () => {
